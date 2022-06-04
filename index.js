64018c4d3746bd24b2a3a5c0bb08c4f86ff2b895
leothorp <leothorp@users.noreply.github.com>
2022-06-04 18:21:11 -0500

    description

---
+++ index.js
@@ -0,0 +1,115 @@
+let initHideOverlay = () => {};
+if (process.env.NODE_ENV !== "production") {
+  console.log("IN HIDE PACKAGE");
+  let observer = null;
+  const defaults = {
+    overlayId: "webpack-dev-server-client-overlay",
+    withRestoreButton: true,
+    disable: false,
+  };
+  console.log("oh");
+
+  //avoids violating CSP
+  const setBtnStyles = (btn, styles) => {
+    Object.entries(styles).forEach(([k, v]) => {
+      btn.style[k] = v;
+    });
+  };
+  const hideOverlay = (node, withRestoreButton) => {
+    const id = node.id;
+    console.log("bts", withRestoreButton);
+
+    if (withRestoreButton) {
+      const btn = document.createElement("button");
+      setBtnStyles(btn, {
+        "background-color": "darkred",
+        opacity: 0.5,
+        width: "100px",
+        "font-size": "14px",
+        padding: "10px 0px",
+        "text-align": "center",
+        position: "fixed",
+        color: "white",
+        right: 0,
+        bottom: 0,
+        cursor: "pointer",
+        "z-index": parseInt(node.style["z-index"]),
+      });
+      btn.id = "hide-cra-error-overlay-btn";
+
+      const maximize = (currOverlayEl, btn) => {
+        if (currOverlayEl) {
+          currOverlayEl.style.display = "block";
+          btn.textContent = "Hide Errors";
+        }
+      };
+      const minimize = (currOverlayEl, btn) => {
+        if (currOverlayEl) {
+          currOverlayEl.style.display = "none";
+          btn.textContent = "Show Errors";
+        }
+      };
+      btn.addEventListener("click", function (e) {
+        const currOverlayEl = document.getElementById(id);
+        const isShown = currOverlayEl.style.display !== "none";
+        if (isShown) {
+          minimize(currOverlayEl, e.target);
+        } else {
+          maximize(currOverlayEl, e.target);
+        }
+      });
+
+      minimize(node, btn);
+      document.body.appendChild(btn);
+
+
+    } else {
+      node.remove();
+    }
+  };
+
+  const _initHideOverlay = (userConfig = {}) => {
+    const config = Object.assign({}, defaults, userConfig);
+    const { overlayId, withRestoreButton, disable } = config;
+    destroyObserver();
+    if (disable) {
+      return;
+    }
+    //TODO: (what happens for wrong id)
+    //TODO: (test prod bundle impact (lib for that?))
+    observer = new MutationObserver((mutationList) => {
+      mutationList.forEach((mutation) => {
+        console.log("mutty", mutation);
+        mutation.addedNodes.forEach((node) => {
+          console.log("added node: ", node);
+          if (node.id === overlayId) {
+            hideOverlay(node, withRestoreButton);
+          }
+        });
+        mutation.removedNodes.forEach((node) => {
+          console.log("rmed node: ", node);
+          if (node.id === overlayId) {
+            const btn = document.getElementById("hide-cra-error-overlay-btn");
+            if (btn) {
+              btn.remove();
+            }
+          }
+        });
+      });
+    });
+    const options = {
+      subtree: false,
+      childList: true,
+    };
+    observer.observe(document.querySelector("body"), options);
+  };
+  const destroyObserver = () => {
+    observer?.disconnect();
+    observer = null;
+  };
+  initHideOverlay = _initHideOverlay;
+  console.log("callin");
+  initHideOverlay();
+}
+
+export { initHideOverlay };
