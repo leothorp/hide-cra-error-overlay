178e21d0770c49975f3f54a54cc4341ff20e4211
leothorp <leothorp@users.noreply.github.com>
2022-07-23 15:44:14 -0600

    require

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
 # hide-cra-error-overlay
 
-### Hide the Create React App error overlay behind a "Show Errors" button. ##
-
-
+### Hide the Create React App error overlay behind a "Show Errors" button.
 
 ### Installation
+
 ```
-  npm i hide-cra-error-overlay
+  npm install --save-dev hide-cra-error-overlay
 ```
+
 ### Usage
-In src/index.js (or another project file):
+
+In src/index.js, add the following:
+
 ```javascript
 if (process.env.NODE_ENV !== "production") {
-  import("hide-cra-error-overlay");
+  import("hide-cra-error-overlay").then(({ initHideOverlay }) =>
+    initHideOverlay()
+  );
 }
 ```
+
 That's it! the overlay will be replaced with the "Show Errors" button from now on.
 
 ### Screenshots
+
 Before (overlay):
 
 <img src="https://user-images.githubusercontent.com/12928449/172033836-5daadd0e-d237-4546-9afa-5244cce43211.png" width="400" height="300">
@@ -27,28 +33,38 @@ After (button):
 <img src="https://user-images.githubusercontent.com/12928449/172033833-0b601939-8a87-4b76-afc9-2520c2554923.png" width="400" height="300">
 
 ### Why would someone use this?
+
 During development, Create React App displays a full-screen error overlay for webpack compilation errors. The config for this is only accessible by ejecting- but particularly with Typescript errors, sometimes you may just want to temporarily ignore these to be able to test your app.
 When imported, this module hides the error overlay whenever it is triggered. It also adds a small "Show Errors" button to the lower right of the screen- click this to toggle the original error overlay on/off at any time.
 
 ### In Production
-Assuming that you follow the recommended usage above, there is zero impact on production bundle size from using this. CRA's webpack config will strip out the contents of the NODE_ENV !== "production" conditional for the production build, and a dynamic import is used to prevent inclusion in the bundle unless that condition is met.
+
+Assuming that you follow the recommended usage above, there is no impact on production bundle size from using this. CRA's webpack config will strip out the contents of the NODE_ENV !== "production" conditional for the production build, and a dynamic import is used to prevent inclusion in the bundle unless that condition is met.
 
 ### Config
-To adjust the behavior, you can optionally import `initHideOverlay` and invoke it with any of the options in the example below. Values shown below are the defaults. Note that before this function is invoked, the default behavior will be in place.
-```javascript
-import {initHideOverlay} from "hide-cra-error-overlay";
 
-initHideOverlay({
-    //DOM id(s) of the 'overlay' element to hide when it appears
-    //(assumed to be a direct child of document.body)
-    //can be a string, or an array of ids to check for in order.
-    overlayId: ["webpack-dev-server-client-overlay-div", "webpack-dev-server-client-overlay"],
-    
-    //whether or not to add the "Show Errors" button
-    withRestoreButton: true,
-   
-   //disable hiding, restore original CRA behavior
-   disable: false, 
-});
+To adjust the behavior, you can optionally pass a configuration object to `initHideOverlay` with any of the options in the example below. Values shown below are the defaults.
+
+```javascript
+if (process.env.NODE_ENV !== "production") {
+  import("hide-cra-error-overlay").then(({ initHideOverlay }) =>
+    initHideOverlay({
+      //HTML id(s) of the 'overlay' element to hide when it appears
+      //(assumed to be a direct child of document.body).
+      //can be a single string id, or an array of multiple ids to check for.
+      overlayId: [
+        "webpack-dev-server-client-overlay",
+        "webpack-dev-server-client-overlay-div",
+      ],
+
+      //whether or not to add the "Show Errors" button
+      withRestoreButton: true,
+
+      //disable hiding, restore original CRA behavior
+      disable: false,
+    })
+  );
+}
 ```
+
 `initHideOverlay` can be invoked multiple times for different config at different points in execution (e.g., when a certain component is mounted).
--- index.js
+++ index.js
@@ -1,142 +1,134 @@
-let initHideOverlay = () => {};
+let observer = null;
+const defaultConfig = {
+  overlayId: [
+    "webpack-dev-server-client-overlay",
+    "webpack-dev-server-client-overlay-div",
+  ],
+  withRestoreButton: true,
+  disable: false,
+};
 
-  let observer = null;
-  const defaults = {
-    overlayId: [
-      // "webpack-dev-server-client-overlay-div",
-      "webpack-dev-server-client-overlay",
-    ],
-    withRestoreButton: true,
-    disable: false,
-  };
+const BUTTON_ID = "hide-cra-error-overlay-btn";
 
-  const BUTTON_ID = "hide-cra-error-overlay-btn";
+//avoids violating CSP
+const setBtnStyles = (btn, styles) => {
+  Object.entries(styles).forEach(([k, v]) => {
+    btn.style[k] = v;
+  });
+};
+const hideOverlay = (node, withRestoreButton) => {
+  const id = node.id;
 
-  //avoids violating CSP
-  const setBtnStyles = (btn, styles) => {
-    Object.entries(styles).forEach(([k, v]) => {
-      btn.style[k] = v;
+  if (withRestoreButton) {
+    const btn = document.createElement("button");
+    setBtnStyles(btn, {
+      "background-color": "darkred",
+      opacity: 0.5,
+      width: "100px",
+      "font-size": "14px",
+      padding: "10px 0px",
+      "text-align": "center",
+      position: "fixed",
+      color: "white",
+      right: 0,
+      bottom: 0,
+      cursor: "pointer",
+      "z-index": parseInt(node.style["z-index"]),
     });
-  };
-  const hideOverlay = (node, withRestoreButton) => {
-    const id = node.id;
+    btn.id = BUTTON_ID;
 
-    if (withRestoreButton) {
-      const btn = document.createElement("button");
-      setBtnStyles(btn, {
-        "background-color": "darkred",
-        opacity: 0.5,
-        width: "100px",
-        "font-size": "14px",
-        padding: "10px 0px",
-        "text-align": "center",
-        position: "fixed",
-        color: "white",
-        right: 0,
-        bottom: 0,
-        cursor: "pointer",
-        "z-index": parseInt(node.style["z-index"]),
-      });
-      btn.id = BUTTON_ID;
-
-      const maximize = (currOverlayEl, btn) => {
-        if (currOverlayEl) {
-          currOverlayEl.style.display = "block";
-          btn.textContent = "Hide Errors";
-        }
-      };
-      const minimize = (currOverlayEl, btn) => {
-        if (currOverlayEl) {
-          currOverlayEl.style.display = "none";
-          btn.textContent = "Show Errors";
-        }
-      };
-      btn.addEventListener("click", function (e) {
-        const currOverlayEl = document.getElementById(id);
-        const isShown = currOverlayEl.style.display !== "none";
-        if (isShown) {
-          minimize(currOverlayEl, e.target);
-        } else {
-          maximize(currOverlayEl, e.target);
-        }
-      });
-
-      minimize(node, btn);
-      document.body.appendChild(btn);
-    } else {
-      node.remove();
-    }
-  };
-
-  const getElByOneOfIds = (ids) => {
-    for (let i = 0; i < ids.length; i++) {
-      const foundEl = document.getElementById(ids[i]);
-      if (foundEl) {
-        console.log('found', foundEl)
-        return foundEl;
-      }
-    }
-  };
-
-  const _initHideOverlay = (userConfig = {}) => {
-
-    const config = Object.assign({}, defaults, userConfig);
-    const { overlayId, withRestoreButton, disable } = config;
-    const normalizedOverlayIds = Array.isArray(overlayId)
-      ? overlayId
-      : [overlayId];
-    destroyObserver();
-
-    if (disable) {
-      const currOverlayEl = getElByOneOfIds(normalizedOverlayIds);
+    const maximize = (currOverlayEl, btn) => {
       if (currOverlayEl) {
         currOverlayEl.style.display = "block";
+        btn.textContent = "Hide Errors";
       }
-      const currButtonEl = document.getElementById(BUTTON_ID);
-      if (currButtonEl) {
-        currButtonEl.remove();
+    };
+    const minimize = (currOverlayEl, btn) => {
+      if (currOverlayEl) {
+        currOverlayEl.style.display = "none";
+        btn.textContent = "Show Errors";
       }
-      return;
-    }
-
-    observer = new MutationObserver((mutationList) => {
-      mutationList.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-          if (normalizedOverlayIds.includes(node.id)) {
-            hideOverlay(node, withRestoreButton);
-          }
-        });
-        mutation.removedNodes.forEach((node) => {
-          if (normalizedOverlayIds.includes(node.id)) {
-            const btn = document.getElementById(BUTTON_ID);
-            if (btn) {
-              btn.remove();
-            }
-          }
-        });
-      });
+    };
+    btn.addEventListener("click", function (e) {
+      const currOverlayEl = document.getElementById(id);
+      const isShown = currOverlayEl.style.display !== "none";
+      if (isShown) {
+        minimize(currOverlayEl, e.target);
+      } else {
+        maximize(currOverlayEl, e.target);
+      }
     });
 
+    minimize(node, btn);
+    document.body.appendChild(btn);
+  } else {
+    node.remove();
+  }
+};
 
-
-    const initialOverlayEl = getElByOneOfIds(normalizedOverlayIds);
-    if (initialOverlayEl) {
-      hideOverlay(initialOverlayEl, withRestoreButton)
+const getElByOneOfIds = (ids) => {
+  for (let i = 0; i < ids.length; i++) {
+    const foundEl = document.getElementById(ids[i]);
+    if (foundEl) {
+      console.log("found", foundEl);
+      return foundEl;
     }
-  
+  }
+};
 
-    const options = {
-      subtree: false,
-      childList: true,
-    };
-    observer.observe(document.body, options);
-  };
-  const destroyObserver = () => {
-    observer?.disconnect();
-    observer = null;
-  };
-  initHideOverlay = _initHideOverlay;
-  initHideOverlay();
+const initHideOverlay = (userConfig = {}) => {
+  const config = Object.assign({}, defaultConfig, userConfig);
+  const { overlayId, withRestoreButton, disable } = config;
+  const normalizedOverlayIds = Array.isArray(overlayId)
+    ? overlayId
+    : [overlayId];
+  destroyObserver();
 
+  if (disable) {
+    const currOverlayEl = getElByOneOfIds(normalizedOverlayIds);
+    if (currOverlayEl) {
+      currOverlayEl.style.display = "block";
+    }
+    const currButtonEl = document.getElementById(BUTTON_ID);
+    if (currButtonEl) {
+      currButtonEl.remove();
+    }
+    return;
+  }
+
+  observer = new MutationObserver((mutationList) => {
+    mutationList.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (normalizedOverlayIds.includes(node.id)) {
+          hideOverlay(node, withRestoreButton);
+        }
+      });
+      mutation.removedNodes.forEach((node) => {
+        if (normalizedOverlayIds.includes(node.id)) {
+          const btn = document.getElementById(BUTTON_ID);
+          if (btn) {
+            btn.remove();
+          }
+        }
+      });
+    });
+  });
+
+  const initialOverlayEl = getElByOneOfIds(normalizedOverlayIds);
+  if (initialOverlayEl) {
+    hideOverlay(initialOverlayEl, withRestoreButton);
+  }
+
+  const options = {
+    subtree: false,
+    childList: true,
+  };
+  observer.observe(document.body, options);
+};
+
+const destroyObserver = () => {
+  observer?.disconnect();
+  observer = null;
+};
 
 export { initHideOverlay };
